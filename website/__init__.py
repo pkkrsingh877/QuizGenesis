8dbfc2d47412bb6695e89439588b59a52fbde29c
Prabhat Kumar <pkkrsingh877@gmail.com>
2024-05-07 14:27:59 +0530

    Basic Setup for QuizGenesis

---
+++ website/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY") #Encrypts Cookies and Sessions
+    
+    return app
+
+create_app()
